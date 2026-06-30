eed8667de377646910b353f9e1e011b21bd52d05
PedroLSena <pes.ex20000@gmail.com>
2026-06-30 20:38:22 -0300

    Controller fucnionando corretamente com a estrutura de ID simples e service criado e funcionando junto com o controller

--- src/audio/audio.controller.ts
+++ src/audio/audio.controller.ts
@@ -1,11 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { AudioService } from './audio.service';
+
 
 @Controller('audio')
 export class AudioController {
-  @Get()
-  get() {
-    return {
-      message: 'API funcionando !!',
-    };
-  }
+  constructor(private readonly AudioService: AudioService){}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    storage:diskStorage({
+       destination: "./uploads",
+       filename:(req, file, callback)=>{
+        const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
+
+        const ext = extname(file.originalname);
+        const filename  = `${uniqueName}${ext}`
+
+        callback(null, filename)
+       }
+    })
+  }))
+  uploadFile(@UploadedFile() file: Express.Multer.File){ return this.AudioService.precessUpload(file)}
 }
